1f711db2bcc301fb5b43e50e2dbc6e4517f4f9d6
sudarev <sudarevri@gmail.com>
2022-05-22 22:18:08 +0200

    init

---
+++ src/main/kotlin/my/test/dkb/DkbApplication.kt
@@ -0,0 +1,11 @@
+package my.test.dkb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DkbApplication
+
+fun main(args: Array<String>) {
+	runApplication<DkbApplication>(*args)
+}
